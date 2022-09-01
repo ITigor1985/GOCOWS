d5d6eca25df1c4484304b311edf11600fba1c18b
ITigor1985 <81867285+ITigor1985@users.noreply.github.com>
2022-09-01 17:45:56 +0000

    Deploying to gh-pages from @ ITigor1985/GOCOWS@231d6585593e374c231c4d3bc35d1537f41384ca 🚀

---
+++ src.db018ff3.js
@@ -0,0 +1,20 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"N8cg":[function(require,module,exports) {
+
+},{"./material-icons.woff2":[["material-icons.fd4214fa.woff2","jrbM"],"jrbM"],"./material-icons.woff":[["material-icons.febcf3b0.woff","Yvfm"],"Yvfm"]}],"clu1":[function(require,module,exports) {
+
+},{"./../fonts/Roboto-Regular.woff":[["Roboto-Regular.1a673faa.woff","xTI8"],"xTI8"],"./../fonts/Roboto-Regular.woff2":[["Roboto-Regular.f1277972.woff2","zexy"],"zexy"],"./../fonts/Roboto-Medium.woff":[["Roboto-Medium.4173280d.woff","aGep"],"aGep"],"./../fonts/Roboto-Medium.woff2":[["Roboto-Medium.8243b2e3.woff2","Uybm"],"Uybm"]}],"C1JV":[function(require,module,exports) {
+$(document).ready(function(){$(".reviews__list").slick({slidesToShow:1,slidesToScroll:1,speed:1e3,easing:"ease"})});const e=document.querySelectorAll(".reviews__review"),t=document.body.clientWidth;e.forEach(e=>{let l=e.clientHeight;t<=767&&parseInt(l)>=173&&(e.style.height="172px",e.style.overflow="auto"),t>=768&&parseInt(l)>=126&&(e.style.height="210px",e.style.overflow="auto")});
+},{}],"dE7Y":[function(require,module,exports) {
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-shown")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+},{}],"HybV":[function(require,module,exports) {
+(()=>{const e={openModalVideoBtn:document.querySelector(".feature__button"),closeModalVideoBtn:document.querySelector("[data-modal-close-video]"),modalVideo:document.querySelector("[data-video]"),video:document.querySelector("video")};function o(){e.modalVideo.classList.toggle("is-shown")}e.openModalVideoBtn.addEventListener("click",o),e.closeModalVideoBtn.addEventListener("click",o),e.closeModalVideoBtn.addEventListener("click",function(){e.video.pause(),e.video.currentTime=0})})();
+},{}],"cDEs":[function(require,module,exports) {
+$(document).ready(function(){const o=document.body.clientWidth;o>=768&&o<=1440&&$(".work__list").slick({infinite:!1,dots:!1,arrows:!1,slidesToShow:5,slidesToScroll:5})});
+},{}],"chxP":[function(require,module,exports) {
+$(document).ready(function(){$(".gallery__list").slick({slidesToShow:2,slidesToScroll:1,speed:1e3,easing:"ease",responsive:[{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]})});
+},{}],"DnZU":[function(require,module,exports) {
+const c=document.querySelectorAll(".faq");c.forEach(c=>{c.addEventListener("click",function(){c.classList.toggle("active")})});
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("material-icons/iconfont/filled.css"),require("./sass/main.scss"),require("./js/reviews"),require("./js/backdrop"),require("./js/video"),require("./js/slick"),require("./js/gallery"),require("./js/faq");
+},{"material-icons/iconfont/filled.css":"N8cg","./sass/main.scss":"clu1","./js/reviews":"C1JV","./js/backdrop":"dE7Y","./js/video":"HybV","./js/slick":"cDEs","./js/gallery":"chxP","./js/faq":"DnZU"}]},{},["Focm"], null)
+//# sourceMappingURL=src.db018ff3.js.map
