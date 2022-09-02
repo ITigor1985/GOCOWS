b3885b5d0cb2bc8a69e4fc815424f51bea24a492
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-09-02 19:13:56 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import 'material-icons/iconfont/filled.css';
 import './sass/main.scss';
 
-// import './js/script';
-import './js/reviews';
-//import './js/backdrop';
-//import './js/video';
-import './js/slick';
-
 import './js/header';
 import './js/hero';
